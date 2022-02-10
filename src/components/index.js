be762aa465beba13d1d076189ca95d06ca37d965
n-kev1n <kevin_@tuta.io>
2022-02-10 17:04:16 +0100

    components holder

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Nav } from "./Nav/Nav";
+export { default as Hero } from "./Hero/Hero";
+export { default as StoryCards } from "./StoryCards/StoryCards";
+export { default as Footer } from "./Footer/Footer";
